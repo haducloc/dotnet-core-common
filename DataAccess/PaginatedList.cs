fa7b6c094c5a3dc025a9417216b41f0d50e980be
Loc Ha <haducloc13@gmail.com>
2018-10-25 14:16:09 -0500

    Add PaginatedList

---
+++ DataAccess/PaginatedList.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.Common.DataAccess
+{
+    public class PaginatedList<T>
+    {
+        // >= 1
+        public int PageIndex { get; private set; }
+
+        public int TotalPages { get; private set; }
+
+        public IList<T> Items { get; private set; }
+
+        public PaginatedList(IList<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.Items = items;
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int? count, int pageIndex, int pageSize)
+        {
+            var countVal = count ?? await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PaginatedList<T>(items, countVal, pageIndex, pageSize);
+        }
+    }
+}
