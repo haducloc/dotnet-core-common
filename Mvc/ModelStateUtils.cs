bef8fbc28ff377af083320aec6ead9a20e3b3b5e
Loc Ha <loc.ha@Nebraska.gov>
2018-11-01 15:19:16 -0500

    Add ToErrorMap

--- Mvc/ModelStateUtils.cs
+++ Mvc/ModelStateUtils.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using NetCore.Common.Utils;
+using System.Collections.Generic;
 
 namespace NetCore.Common.Mvc
 {
@@ -8,5 +9,24 @@ namespace NetCore.Common.Mvc
         public static void AddFormError(this ModelStateDictionary modelState, string errorMessage) => modelState.AddModelError("", errorMessage);
 
         public static void RemoveErrors(this ModelStateDictionary modelState, params string[] fieldNames) => fieldNames.ForEach(fieldName => modelState.Remove(fieldName));
+
+        public static Dictionary<string, IList<string>> ToErrorMap(this ModelStateDictionary modelState)
+        {
+            var map = new Dictionary<string, IList<string>>();
+
+            modelState.Keys.ForEach(key =>
+            {
+                var entry = modelState[key];
+                var list = new List<string>(entry.Errors.Count);
+
+                entry.Errors.ForEach(error =>
+                {
+                    list.Add(error.ErrorMessage);
+                });
+
+                map[key] = list;
+            });
+            return map;
+        }
     }
 }
