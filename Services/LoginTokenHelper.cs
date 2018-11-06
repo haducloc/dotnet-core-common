f66ba58614509d42e280a4312072d7006f06bb4d
Loc Ha <loc.ha@Nebraska.gov>
2018-11-06 11:17:45 -0600

    Add LoginTokenHelper

---
+++ Services/LoginTokenHelper.cs
@@ -0,0 +1,22 @@
+﻿using NetCore.Common.Crypto;
+using NetCore.Common.Models;
+using NetCore.Common.Utils;
+using Newtonsoft.Json;
+
+namespace NetCore.Common.Services
+{
+    public class LoginTokenHelper
+    {
+        public static string GenerateToken(LoginToken loginToken, TextEncryptor signer)
+        {
+            AssertUtils.AssertNotNull(loginToken);
+            return signer.Encrypt(JsonConvert.SerializeObject(loginToken));
+        }
+
+        public static LoginToken ParseToken(string loginToken, TextEncryptor signer)
+        {
+            AssertUtils.AssertNotNull(loginToken);
+            return JsonConvert.DeserializeObject<LoginToken>(signer.Decrypt(loginToken));
+        }
+    }
+}
